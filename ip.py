f36cd8ad2d3d3bb07c01c489b4264d56cbd4bf36
zke <85144998+zkeandrhia@users.noreply.github.com>
2021-07-16 18:52:08 +0800

    Add files via upload

---
+++ ip.py
@@ -0,0 +1,10 @@
+name = input('Name > ')
+age = input('Age > ')
+tpn = input('Adress > ')
+cm = input('Major in > ')
+
+print(f'''
+          Name: {name}
+          Age: {age} yearsold.
+          Adress: {tpn}
+          Major in: {cm} ''')
